1d023a9c5fad90dbc225f9f7f78dd6ed0588182c
thanush-123 <gthanushika1@gmail.com>
2023-09-17 18:47:34 +0530

    donars page fist version

--- Orphanage_Care_Manager/DonarsViewForm.cs
+++ Orphanage_Care_Manager/DonarsViewForm.cs
@@ -1,4 +1,5 @@
-﻿using Orphanage_Care_Manager.model;
+﻿using Orphanage_Care_Manager.db;
+using Orphanage_Care_Manager.model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,24 +11,31 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
+
 namespace Orphanage_Care_Manager
 {
     public partial class DonarsViewForm : Form
     {
         Admin admin;
         String status;
+        private ErrorProvider errorProvider;
         public DonarsViewForm(Admin admin,String status)
         {
             InitializeComponent();
             this.admin = admin;
             this.status = status;
             restrictComponents();
-           
+            errorProvider = new ErrorProvider();
+
         }
 
         private void DonarsViewForm_Load(object sender, EventArgs e)
         {
-          
+            errorName.Text = "";
+            errorNIC.Text = "";
+            errorEmail.Text = "";
+            errorContact.Text = "";
+            errorType.Text = "";
 
         }
 
@@ -38,6 +46,64 @@ namespace Orphanage_Care_Manager
 
         private void panel2_Paint(object sender, PaintEventArgs e)
         {
+
+        }
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Boolean validation = true;
+            if (string.IsNullOrEmpty(txtNic.Text.Trim()))
+            {
+                //txtNic.Text = "Please Enter NIC";
+                //txtNic.ForeColor = Color.IndianRed;
+                errorNIC.Text = "required";
+                errorNIC.ForeColor = Color.IndianRed;
+                validation = false;
+            }
+            if (string.IsNullOrEmpty(txtName.Text.Trim()))
+            {
+                //txtName.Text = "Please Enter NIC";
+                //txtName.ForeColor = Color.IndianRed;
+                errorName.Text = "required";
+                errorName.ForeColor = Color.IndianRed;
+                validation = false;
+            }
+            if (string.IsNullOrEmpty(txtContact.Text.Trim()))
+            {
+                //txtContact.Text = "Please Enter NIC";
+                //txtContact.ForeColor = Color.IndianRed;
+                errorContact.Text = "required";
+                errorContact.ForeColor = Color.IndianRed;
+                validation = false;
+            }
+            if (string.IsNullOrEmpty(txtEmail.Text.Trim()))
+            {
+                //txtEmail.Text = "Please Enter NIC";
+                //txtEmail.ForeColor = Color.IndianRed;
+                errorEmail.Text = "required";
+                errorEmail.ForeColor = Color.IndianRed;
+                validation = false;
+            }
+             if (string.IsNullOrEmpty(txtDonarType.Text.Trim()))
+            {
+                //txtDonarType.Text = "Please Enter NIC";
+                //txtDonarType.ForeColor = Color.IndianRed;
+                errorType.Text = "required";
+                errorType.ForeColor = Color.IndianRed;
+                validation = false;
+            }
+            if(validation==true)
+            {
+                Admin admin1 = new Admin();
+                admin1.UserName = "Gavesh";
+                Donar donar = new Donar(txtNic.Text, txtName.Text, txtContact.Text, txtEmail.Text, txtDonarType.Text, admin1);
+                DonarsQuery donarsQuery = new DonarsQuery();
+                donarsQuery.addDonar(donar);
+
+            }
+            
+         
 
         }
 
@@ -72,9 +138,50 @@ namespace Orphanage_Care_Manager
 
         }
 
-        private void btnAdd_Click(object sender, EventArgs e)
+        
+        private void textBox_Validating(object sender, CancelEventArgs e)
+        {
+            
+
+            if (string.IsNullOrWhiteSpace(txtNic.Text))
+            {
+                errorProvider.SetError(txtNic, "Name is required.");
+                e.Cancel = true;
+            }
+            else
+            {
+                errorProvider.SetError(txtNic, ""); // Clear error message
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void load_form(Form newForm)
+        {
+            if (newForm != null && newForm is Form)
+            {
+                Form currentForm = this.Parent.Controls.OfType<Form>().FirstOrDefault();
+                if (currentForm != null)
+                {
+                    newForm.Dock = DockStyle.Fill;
+                    newForm.TopLevel = false;
+                    currentForm.Hide();
+                    this.Parent.Controls.Add(newForm);
+                }
+                newForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid form parameter.");
+            }
         }
     }
 }
---
+++ Orphanage_Care_Manager/db/DonarsQuery.cs
@@ -0,0 +1,106 @@
+﻿using Orphanage_Care_Manager.model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Orphanage_Care_Manager.db
+{
+    class DonarsQuery
+    {
+        private Donar donar;
+        private Admin admin;
+        public DonarsQuery()
+        {
+          
+
+        }
+
+
+        public void addDonar(Donar donar)
+        {
+
+
+            this.donar = donar;
+            try
+            {
+                // Your code to insert data into the database goes here
+                SqlConnection conn = DBConnection.getConnection();
+                string sqlString = "insert into dbo.Donator values(@donarNIC,@donarName,@donarContact,@donarEmail,@donarType,@admin)";
+                using (SqlCommand cmd = new SqlCommand(sqlString, conn))
+                {
+                    cmd.Parameters.AddWithValue("@donarNIC", donar.Nic);
+                    cmd.Parameters.AddWithValue("@donarName", donar.Name);
+                    cmd.Parameters.AddWithValue("@donarContact", donar.Contact);
+                    cmd.Parameters.AddWithValue("@donarEmail", donar.Email);
+                    cmd.Parameters.AddWithValue("@donarType", donar.Type);
+                    cmd.Parameters.AddWithValue("@admin", donar.Admin.UserName);
+
+                    int affectedRows = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException ex)
+            {
+                if (ex.Number == 2627) // Error number for a unique key violation
+                {
+                    // Handle the duplicate key violation here
+                    MessageBox.Show("A record with this key already exists.");
+                }
+                else
+                {
+                    // Handle other SQL Server exceptions
+                    MessageBox.Show("An error occurred: " + ex.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle other exceptions (not related to SQL Server)
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+
+
+        }
+
+        public void retriveDonars(DataTable dt)
+        {
+            string sqlString = "select * from dbo.Donator";
+            using (SqlCommand cmd = new SqlCommand(sqlString, DBConnection.getConnection()))
+            {
+               using (SqlDataAdapter adapter=new SqlDataAdapter(cmd))
+                {
+                    adapter.Fill(dt);
+                }
+                cmd.ExecuteNonQuery();
+            };
+            
+        }
+        public void updateDonar(Donar donar)
+        {
+            this.donar = donar;
+        }
+        public void deleteDonar(Donar donar)
+        {
+            this.donar = donar;
+            try
+            {
+                string sqlString = "DELETE FROM dbo.Donator WHERE PrimaryKeyColumn = @PrimaryKeyValue";
+                using (SqlCommand cmd = new SqlCommand(sqlString, DBConnection.getConnection()))
+                {
+                    cmd.Parameters.Add(donar.Nic);
+                    cmd.ExecuteNonQuery();
+                };
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("SQL Exception: "+ex.Message);
+            }
+
+
+        }
+
+    }
+}
